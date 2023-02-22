ae188894ce160dcf57bf707dbb2248da342c4ee6
root <lowlevelengineer@gmail.com>
2023-02-23 00:09:27 +0900

    origin

--- program2-2.py
+++ program2-2.py
@@ -1,3 +1,19 @@
 
 num_lines = int(input())
+R = [0] * num_lines
 
+i = 0
+while i < num_lines:
+    R[i] = int(input())
+    i += 1
+    
+maxv = -2000000
+minv = R[0]
+
+i = 0
+while i < num_lines:
+    maxv = max(maxv, R[i] - minv)
+    minv = min(minv, R[i])
+    i += 1
+
+print(maxv)
