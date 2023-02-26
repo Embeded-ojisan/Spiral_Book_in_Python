23474fcf9b20b4ca9a48f83c25f4d5338723220a
root <lowlevelengineer@gmail.com>
2023-02-26 22:56:39 +0900

    origin

---
+++ program3-5.py
@@ -0,0 +1,16 @@
+
+class Card:
+    def __init(suit, value):
+        self.suit = suit
+        self.value = value
+
+############ main ############
+N = int(input())
+A = [0] * 100
+i = 0
+x = 0
+
+X = input().split()
+
+for x in X:
+    A[i] = int
