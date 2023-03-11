a60cb6d568c427768a0bd25016e161af33701baf
root <lowlevelengineer@gmail.com>
2023-03-12 00:47:25 +0900

    origin

--- program3-6.py
+++ program3-6.py
@@ -1,5 +1,29 @@
 import sys
 
+cnt=0
+G=[0]
+
+def insertionSort(A, n, g):
+    for i in range(g, n):
+        v = A[i]
+        j = i - g
+    while j >= 0 and A[j] > v:
+        A[j + g] = A[j]
+        j -= g
+        cnt += 1
+    A[j+g]=v
+
+def shellSort(A, n):
+    h=1
+    while True:
+        if h > n:
+            break
+        G.append(h)
+        h = 3*h + 1
+
+    for i in reversed(range(0, G.size())):
+        insertionSort(A, n, G[i])
+
 ############ main ############
 n = int(input())
 i = 0
